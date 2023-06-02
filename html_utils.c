ef2a21c0299abd6b44dd3343302d1751a3fb470f
Brian <gisameht@gmail.com>
2023-06-02 01:29:46 -0400

    detalles css

--- html_utils.c
+++ html_utils.c
@@ -7,9 +7,11 @@
 
 char mod_str[80];
 
-char *read_file(const char *filename) {
+char *read_file(const char *filename)
+{
     FILE *file = fopen(filename, "rb");
-    if (!file) {
+    if (!file)
+    {
         perror("fopen");
         return NULL;
     }
@@ -21,21 +23,25 @@ char *read_file(const char *filename) {
 
     // Leer el archivo en bloques
     char *buffer = malloc(size + 1);
-    if (!buffer) {
+    if (!buffer)
+    {
         perror("malloc");
         fclose(file);
         return NULL;
     }
 
     size_t bytes_read = 0;
-    while (bytes_read < size) {
+    while (bytes_read < size)
+    {
         size_t bytes_to_read = size - bytes_read;
-        if (bytes_to_read > 1024) {
+        if (bytes_to_read > 1024)
+        {
             bytes_to_read = 1024;
         }
 
         size_t bytes = fread(buffer + bytes_read, 1, bytes_to_read, file);
-        if (bytes == 0) {
+        if (bytes == 0)
+        {
             break;
         }
 
@@ -49,91 +55,97 @@ char *read_file(const char *filename) {
     return buffer;
 }
 
-char *permissions(int id) {
-    switch (id % 8) {
-        case 0:
-            return "No permission";
-        case 1:
-            return "Execute";
-        case 2:
-            return "Write";
-        case 3:
-            return "Write and execute";
-        case 4:
-            return "Read";
-        case 5:
-            return "Read and execute";
-        case 6:
-            return "Read and write";
-        case 7:
-            return "Read, write and execute";
-        default:
-            return "-";
+char *permissions(int id)
+{
+    switch (id % 8)
+    {
+    case 0:
+        return "No permission";
+    case 1:
+        return "Execute";
+    case 2:
+        return "Write";
+    case 3:
+        return "Write and execute";
+    case 4:
+        return "Read";
+    case 5:
+        return "Read and execute";
+    case 6:
+        return "Read and write";
+    case 7:
+        return "Read, write and execute";
+    default:
+        return "-";
     }
 }
 
-char *time_stat(struct stat info) {
+char *time_stat(struct stat info)
+{
     time_t mod_time = info.st_mtime;
     struct tm *mod_tm = localtime(&mod_time);
     strftime(mod_str, 80, "%Y-%m-%d %A %H:%M:%S", mod_tm);
     return mod_str;
 }
 
-void html_builder(int client_socket, const char *root_dir) {
+void html_builder(int client_socket, const char *root_dir)
+{
     char response[8192];
     snprintf(response, sizeof(response),
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/html\r\n\r\n"
-        "<!DOCTYPE html>\n<html>\n<head>\n");
-    
+             "HTTP/1.1 200 OK\r\n"
+             "Content-Type: text/html\r\n\r\n"
+             "<!DOCTYPE html>\n<html>\n<head>\n");
+
     DIR *dir;
     struct dirent *entry;
 
     // abrir directorio
     dir = opendir(root_dir);
-    if (!dir) {
+    if (!dir)
+    {
         snprintf(response, sizeof(response),
-            "HTTP/1.1 403 Forbidden\r\n"
-            "Content-Type: text/html\r\n\r\n"
-            "<!DOCTYPE html>\n<html>\n<head>\n"
-            "<title>Error 403: Acceso denegado</title>\n"
-            "</head>\n<body>\n"
-            "<h1>Error 403: Acceso denegado</h1>\n"
-            "<p>No tienes permisos suficientes para acceder al directorio especificado.</p>\n"
-            "</body>\n</html>");
+                 "HTTP/1.1 403 Forbidden\r\n"
+                 "Content-Type: text/html\r\n\r\n"
+                 "<!DOCTYPE html>\n<html>\n<head>\n"
+                 "<title>Error 403: Acceso denegado</title>\n"
+                 "</head>\n<body>\n"
+                 "<h1>Error 403: Acceso denegado</h1>\n"
+                 "<p>No tienes permisos suficientes para acceder al directorio especificado.</p>\n"
+                 "</body>\n</html>");
         send(client_socket, response, strlen(response), 0);
         return;
     }
 
     // escribir encabezado HTML
     snprintf(response + strlen(response), sizeof(response) - strlen(response),
-        "<title>Contenido de %s</title>\n", root_dir);
+             "<title>Contenido de %s</title>\n", root_dir);
     snprintf(response + strlen(response), sizeof(response) - strlen(response),
-        "<style>\n%s\n</style>\n", read_file(".styles.css"));
+             "<style>\n %s \n</style>\n", read_file(".styles.css"));
     snprintf(response + strlen(response), sizeof(response) - strlen(response),
-        "<script>\nfunction sortTable(n) {\nvar table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;\n"
-        "table = document.getElementsByTagName(\"table\")[0];\nswitching = true;\ndir = \"asc\";\nwhile (switching) {\n"
-        "switching = false;\nrows = table.rows;\nfor (i = 1; i < (rows.length - 1); i++) {\n"
-        "shouldSwitch = false;\nx = rows[i].getElementsByTagName(\"td\")[n];\n"
-        "y = rows[i + 1].getElementsByTagName(\"td\")[n];\nif (dir == \"asc\") {\n"
-        "if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {\nshouldSwitch = true;\nbreak;\n}\n"
-        "} else if (dir == \"desc\") {\nif (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {\n"
-        "shouldSwitch = true;\nbreak;\n}\n}\n}\nif (shouldSwitch) {\nrows[i].parentNode.insertBefore(rows[i + 1], rows[i]);\n"
-        "switching = true;\nswitchcount ++;\n} else {\nif (switchcount == 0 && dir == \"asc\") {\n"
-        "dir = \"desc\";\nswitching = true;\n}\n}\n}\n}\n</script>\n");
+             "<script>\nfunction sortTable(n) {\nvar table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;\n"
+             "table = document.getElementsByTagName(\"table\")[0];\nswitching = true;\ndir = \"asc\";\nwhile (switching) {\n"
+             "switching = false;\nrows = table.rows;\nfor (i = 1; i < (rows.length - 1); i++) {\n"
+             "shouldSwitch = false;\nx = rows[i].getElementsByTagName(\"td\")[n];\n"
+             "y = rows[i + 1].getElementsByTagName(\"td\")[n];\nif (dir == \"asc\") {\n"
+             "if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {\nshouldSwitch = true;\nbreak;\n}\n"
+             "} else if (dir == \"desc\") {\nif (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {\n"
+             "shouldSwitch = true;\nbreak;\n}\n}\n}\nif (shouldSwitch) {\nrows[i].parentNode.insertBefore(rows[i + 1], rows[i]);\n"
+             "switching = true;\nswitchcount ++;\n} else {\nif (switchcount == 0 && dir == \"asc\") {\n"
+             "dir = \"desc\";\nswitching = true;\n}\n}\n}\n}\n</script>\n");
     snprintf(response + strlen(response), sizeof(response) - strlen(response),
-        "</head>\n<body>\n<h1>\n<center>\nContenido de %s\n</center>\n</h1>\n<table>\n", root_dir);
+             "</head>\n<body>\n<h1>\n<center>\nContenido de %s\n</center>\n</h1>\n<table>\n", root_dir);
     snprintf(response + strlen(response), sizeof(response) - strlen(response),
-        "<thead>\n<tr>\n<th id=\"nombre\" onclick=\"sortTable(0)\">Name</th>\n");
+             "<thead>\n<tr>\n<th id=\"nombre\" onclick=\"sortTable(0)\">Name</th>\n");
     snprintf(response + strlen(response), sizeof(response) - strlen(response),
-        "<th id=\"peso\" onclick=\"sortTable(1)\">Size</th>\n");
+             "<th id=\"peso\" onclick=\"sortTable(1)\">Size</th>\n");
     snprintf(response + strlen(response), sizeof(response) - strlen(response),
-        "<th id=\"fecha\" onclick=\"sortTable(2)\">Date</th>\n");
+             "<th id=\"fecha\" onclick=\"sortTable(2)\">Date</th>\n");
     snprintf(response + strlen(response), sizeof(response) - strlen(response),
-        "<th id=\"permisos\" onclick=\"sortTable(3)\">Permission</th>\n</tr>\n</thead>\n<tbody>\n");
+             "<th id=\"permisos\" onclick=\"sortTable(3)\">Permission</th>\n</tr>\n</thead>\n<tbody>\n");
 
     // escribir lista de archivos y subdirectorios
-    while ((entry = readdir(dir)) != NULL) {
+    while ((entry = readdir(dir)) != NULL)
+    {
         // obtener ruta completa del archivo o directorio
         char entry_path[1024];
         snprintf(entry_path, sizeof(entry_path), "%s/%s", root_dir, entry->d_name);
@@ -141,33 +153,35 @@ void html_builder(int client_socket, const char *root_dir) {
         // determinar si es un archivo o un directorio
         int is_dir = 0;
         DIR *subdir = opendir(entry_path);
-        if (subdir) {
+        if (subdir)
+        {
             is_dir = 1;
             closedir(subdir);
         }
 
         // escribir elemento de lista en el archivo HTML
         struct stat info;
-        if (stat(entry_path, &info) == 0) {
+        if (stat(entry_path, &info) == 0)
+        {
             if (is_dir)
                 snprintf(response + strlen(response), sizeof(response) - strlen(response),
-                    "<tr>\n<td><a href=\"%s/\">%s/</a></td>\n", entry->d_name, entry->d_name);
+                         "<tr>\n<td><a href=\"%s/\">%s/</a></td>\n", entry->d_name, entry->d_name);
             else
                 snprintf(response + strlen(response), sizeof(response) - strlen(response),
-                    "<tr>\n<td><a href=\"%s\">%s</a></td>\n", entry->d_name, entry->d_name);
+                         "<tr>\n<td><a href=\"%s\">%s</a></td>\n", entry->d_name, entry->d_name);
             snprintf(response + strlen(response), sizeof(response) - strlen(response),
-                "<td>%ld bytes</td>\n", info.st_size);
+                     "<td>%ld bytes</td>\n", info.st_size);
             snprintf(response + strlen(response), sizeof(response) - strlen(response),
-                "<td>%s</td>\n", time_stat(info));
+                     "<td>%s</td>\n", time_stat(info));
             snprintf(response + strlen(response), sizeof(response) - strlen(response),
-                "<td>%s</td>\n</tr>\n", permissions(info.st_mode & 0777));
+                     "<td>%s</td>\n</tr>\n", permissions(info.st_mode & 0777));
         }
     }
     closedir(dir);
 
     // escribir cierre HTML
     snprintf(response + strlen(response), sizeof(response) - strlen(response),
-        "</tbody>\n</table>\n</body>\n</html>");
+             "</tbody>\n</table>\n</body>\n</html>");
 
     printf("Archivo HTML generado exitosamente.\n");
 
